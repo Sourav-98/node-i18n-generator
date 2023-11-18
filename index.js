eea22121dea6cec59e7bc689f505bd568798eb54
Sourav Ray Chaudhuri <sourav.raychaudhuri@gmail.com>
2023-11-18 22:42:00 +0530

    i18n generation mods

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const parse = require("csv-parse").parse;
 
-const generateTranslation = async (nameSpace) => {
+const generateTranslation = async (nameSpace, localeCodesConfig) => {
   const commonCsvFile = await fs.promises.readFile(
-    path.resolve(__dirname, `translations/inputCSV/${nameSpace}.csv`),
+    path.resolve(__dirname, `assets/${nameSpace}.csv`),
     { encoding: "utf-8" }
   );
 
@@ -12,118 +12,51 @@ const generateTranslation = async (nameSpace) => {
     commonCsvFile,
     {
       delimiter: ",",
-      columns: ["Keys", "en_US", "en_GB", "de_DE", "fr_FR", "es_ES"],
+      columns: [
+        "Keys",
+        "en_US",
+        "en_GB",
+        "de_DE",
+        "fr_FR",
+        "es_ES",
+        "ar_AR",
+        "ru_RU",
+      ],
     },
     (err, res) => {
       if (err) {
         console.log(err);
         return;
       }
-      const parsedLocale = res.reduce(
-        (accumulator, localeMap) => {
-          accumulator["en_US"][localeMap.Keys] = localeMap.en_US;
-          accumulator["en_GB"][localeMap.Keys] = localeMap.en_GB;
-          accumulator["de_DE"][localeMap.Keys] = localeMap.de_DE;
-          accumulator["fr_FR"][localeMap.Keys] = localeMap.fr_FR;
-          accumulator["es_ES"][localeMap.Keys] = localeMap.es_ES;
-          return accumulator;
-        },
-        {
-          en_US: {},
-          en_GB: {},
-          de_DE: {},
-          fr_FR: {},
-          es_ES: {},
-        }
-      );
-      fs.mkdirSync(path.resolve(__dirname, "translations/outputJSON/en_US"), {
-        recursive: true,
-      });
-      fs.writeFile(
-        path.resolve(
-          __dirname,
-          `translations/outputJSON/en_US/${nameSpace}.json`
-        ),
-        JSON.stringify(parsedLocale.en_US),
-        { encoding: "utf-8" },
-        (err) => {
-          if (err) {
-            console.log('Error parsing for en_US: ', err);
-            return;
+      const parsedLocale = res.slice(1).reduce((accumulator, localeMap) => {
+        localeCodesConfig.forEach((localeCode) => {
+          if (!accumulator[localeCode]) {
+            accumulator[localeCode] = {};
           }
-        }
-      );
-      fs.mkdirSync(path.resolve(__dirname, "translations/outputJSON/en_GB"), {
-        recursive: true,
-      });
-      fs.writeFile(
-        path.resolve(
-          __dirname,
-          `translations/outputJSON/en_GB/${nameSpace}.json`
-        ),
-        JSON.stringify(parsedLocale.en_GB),
-        { encoding: "utf-8" },
-        (err) => {
-          if (err) {
-            console.log('Error parsing for en_GB: ', err);
-            return;
+          accumulator[localeCode][localeMap.Keys] = localeMap[localeCode];
+        });
+        return accumulator;
+      }, {});
+      localeCodesConfig.forEach((localeCode) => {
+        fs.mkdirSync(path.resolve(__dirname, `locales/${localeCode}`), {
+          recursive: true,
+        });
+        fs.writeFile(
+          path.resolve(__dirname, `locales/${localeCode}/${nameSpace}.json`),
+          JSON.stringify(parsedLocale[localeCode]),
+          { encoding: "utf-8" },
+          (err) => {
+            if (err) {
+              console.log(`Error parsing for ${localeCode}: `, err);
+              return;
+            }
           }
-        }
-      );
-      fs.mkdirSync(path.resolve(__dirname, "translations/outputJSON/de_DE"), {
-        recursive: true,
+        );
       });
-      fs.writeFile(
-        path.resolve(
-          __dirname,
-          `translations/outputJSON/de_DE/${nameSpace}.json`
-        ),
-        JSON.stringify(parsedLocale.de_DE),
-        { encoding: "utf-8" },
-        (err) => {
-          if (err) {
-            console.log('Error parsing for de_DE: ', err);
-            return;
-          }
-        }
-      );
-      fs.mkdirSync(path.resolve(__dirname, "translations/outputJSON/fr_FR"), {
-        recursive: true,
-      });
-      fs.writeFile(
-        path.resolve(
-          __dirname,
-          `translations/outputJSON/fr_FR/${nameSpace}.json`
-        ),
-        JSON.stringify(parsedLocale.fr_FR),
-        { encoding: "utf-8" },
-        (err) => {
-          if (err) {
-            console.log('Error parsing for fr_FR: ', err);
-            return;
-          }
-        }
-      );
-      fs.mkdirSync(path.resolve(__dirname, "translations/outputJSON/es_ES"), {
-        recursive: true,
-      });
-      fs.writeFile(
-        path.resolve(
-          __dirname,
-          `translations/outputJSON/es_ES/${nameSpace}.json`
-        ),
-        JSON.stringify(parsedLocale.es_ES),
-        { encoding: "utf-8" },
-        (err) => {
-          if (err) {
-            console.log('Error parsing for es_ES: ', err);
-            return;
-          }
-        }
-      );
     }
   );
 };
 
-generateTranslation("common");
-generateTranslation("textField");
+fs.rmSync(path.resolve(__dirname, "locales"), { recursive: true, force: true });
+generateTranslation("common", process.argv.slice(2));
+generateTranslation("textField", process.argv.slice(2));
